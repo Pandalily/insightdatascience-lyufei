39b9d82cf9cd692ce96b05c8f632930610b22429
Pandalily <lvfei.chen@gmail.com>
2018-11-05 17:20:50 -0500

    Update Lyufei+H1b+stastics.py

--- src/Lyufei+H1b+stastics.py
+++ src/Lyufei+H1b+stastics.py
@@ -51,7 +51,7 @@ Final_Top_10_occupationcode_list = []
 for i in range(len(Top_10_occupationcode_list)):
     a = list(Top_10_occupationcode_list[i])
     Final_Top_10_occupationcode_list.append(a)
-    Final_Top_10_occupationcode_list[i].insert(2,"{:.1%}".format(Top_10_occupationcode_list[i][1]/sum_occupationcode_dict))
+    Final_Top_10_occupationcode_list[i].insert(2,Top_10_occupationcode_list[i][1]/sum_occupationcode_dict)
 print (Final_Top_10_occupationcode_list)
 
 
@@ -113,7 +113,7 @@ Final_Top_10_states_list = []
 for i in range(len(Top_10_states_list)):
     a = list(Top_10_states_list[i])
     Final_Top_10_states_list.append(a)
-    Final_Top_10_states_list[i].insert(2,"{:.1%}".format(Top_10_states_list[i][1]/sum_state_dict))
+    Final_Top_10_states_list[i].insert(2,Top_10_states_list[i][1]/sum_state_dict))
 print (Final_Top_10_states_list)
 
 
