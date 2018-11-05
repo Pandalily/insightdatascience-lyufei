7f2c85643edc86d38e1160a72b10b2123ba2e77b
Pandalily <lvfei.chen@gmail.com>
2018-11-05 17:57:09 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./src/Lyufei+H1b+stastics.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
+python3 ./src/Lyufei+H1b+stastics.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
