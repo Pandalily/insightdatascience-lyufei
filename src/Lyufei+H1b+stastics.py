885a2d9fa389b5f02c0848603a144452e1f857b8
Pandalily <lvfei.chen@gmail.com>
2018-11-05 17:21:46 -0500

    Update Lyufei+H1b+stastics.py

--- src/Lyufei+H1b+stastics.py
+++ src/Lyufei+H1b+stastics.py
@@ -113,7 +113,7 @@ Final_Top_10_states_list = []
 for i in range(len(Top_10_states_list)):
     a = list(Top_10_states_list[i])
     Final_Top_10_states_list.append(a)
-    Final_Top_10_states_list[i].insert(2,Top_10_states_list[i][1]/sum_state_dict))
+    Final_Top_10_states_list[i].insert(2,Top_10_states_list[i][1]/sum_state_dict)
 print (Final_Top_10_states_list)
 
 
